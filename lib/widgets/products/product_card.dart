a474dba506c434c306b67cb9f88e94cbf572c7dc
ncthuan <ncthuan.it@gmail.com>
2021-04-23 10:31:51 +0700

    add product widgets

---
+++ lib/widgets/products/product_card.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+import 'package:scoped_model/scoped_model.dart';
+
+import './price_tag.dart';
+import '../ui_elements/title_default.dart';
+import './address_tag.dart';
+import '../../models/product.dart';
+import '../../scoped-models/main.dart';
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+  final int productIndex;
+
+  ProductCard(this.product, this.productIndex);
+
+  Widget _buildTitlePriceRow() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TitleDefault(product.title),
+          SizedBox(
+            width: 8.0,
+          ),
+          PriceTag(product.price.toString()),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildActionButtons(BuildContext context) {
+    return ButtonBar(
+      alignment: MainAxisAlignment.center,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(Icons.info),
+          color: Theme.of(context).accentColor,
+          onPressed: () => Navigator.pushNamed<bool>(
+              context, '/product/' + productIndex.toString()),
+        ),
+        ScopedModelDescendant<MainModel>(
+            builder: (BuildContext context, Widget child, MainModel model) {
+          return IconButton(
+            icon: Icon(model.products[productIndex].isFavorite 
+            ? Icons.favorite 
+            : Icons.favorite_border),
+            color: Colors.red,
+            onPressed: () {
+              model.selectProduct(productIndex);
+              model.toggleProductFavoriteStatus();
+            },
+          );
+        }),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: new BoxDecoration(boxShadow: [
+        new BoxShadow(
+          color: Colors.grey.withOpacity(0.1),
+          blurRadius: 15.0,
+        ),
+      ]),
+      padding: EdgeInsets.all(10.0),
+      child: Card(
+        child: Column(
+          children: <Widget>[
+            Image.asset(product.image),
+            _buildTitlePriceRow(),
+            AddressTag('Union Square, San Francisco'),
+            Text(product.userEmail),
+            _buildActionButtons(context),
+          ],
+        ),
+      ),
+    );
+  }
+}
